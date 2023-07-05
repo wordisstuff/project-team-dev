9cb934c80bbcf5040d1434346bc77635fc856e78
wordisstuff <wordisstuff@gmail.com>
2023-07-05 07:17:29 +0300

    pull6

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,7 +1,17 @@
 (() => {
+
   const buttonMenu = document.querySelector('.js-btn-hide');
   const onHideBtn = document.querySelector('.js-hide-on');
   const offHideBtn = document.querySelector('.js-hide-off');
+
+  const toggleButton = () => {
+    buttonMenu.classList.toggle('is-hide');
+  };
+  onHideBtn.addEventListener('click', toggleButton);
+  offHideBtn.addEventListener('click', toggleButton);
+
+
+
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
@@ -10,13 +20,23 @@
     mobileMenu.classList.toggle('is-open');
   };
 
-  const toggleButton = () => {
-    buttonMenu.classList.toggle('is-hide');
-  };
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-  onHideBtn.addEventListener('click', toggleButton);
-  offHideBtn.addEventListener('click', toggleButton);
+
+  // const buttonModal = document.querySelector('.js-modal');
+  // const onModalBtn = document.querySelector('.js-modal-on');
+  // const offModalBtn = document.querySelector('.js-modal-off');
+
+  // const toggleModal = () => {
+  //   buttonModal.classList.toggle('is-hidden');
+  // };
+  // const toggleModalClose = () => {
+  //   buttonModal.classList.remove('is-hidden');
+  // };
+
+  // onModalBtn.addEventListener('click', toggleModal);
+  // offModalBtn.addEventListener('click', toggleModalClose);
+
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,10 +1,14 @@
 (() => {
   const refs = {
+    openModalBtnHederMenu: document.querySelector("[heder-menu-modal-open]"),
+    openModalBtnHeder: document.querySelector("[heder-modal-open]"),
     openModalBtn: document.querySelector("[data-modal-open]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
+  refs.openModalBtnHederMenu.addEventListener("click", toggleModal);
+  refs.openModalBtnHeder.addEventListener("click", toggleModal);
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
